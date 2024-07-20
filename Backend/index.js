ff9e0e29226fce55806269987b88dfcfe5daa5aa
siddharthsharma96 <sid9nov@gmail.com>
2024-07-20 11:13:00 +0530

    Databse

--- Backend/index.js
+++ Backend/index.js
@@ -1,6 +1,28 @@
 const express = require("express");
 const cors = require("cors");
 const bodyParser = require("body-parser");
+const mongoose = require("mongoose");
+
+mongoose.connect(
+  "mongodb+srv://karpagam:karpagam1234@cluster0.ebltab1.mongodb.net/"
+);
+const db = mongoose.connection;
+db.on("error", () => {
+  console.log("error connecting database");
+});
+db.once("open", () => {
+  console.log("Connection to db is succesful");
+});
+
+const ExpenseSchema = new mongoose.Schema({
+  amount: Number,
+  type: Number,
+  title: String,
+  date: String,
+  description: String,
+});
+const exp = mongoose.model("Data", ExpenseSchema);
+
 const App = express();
 App.use(cors());
 App.use(bodyParser.json());
@@ -14,21 +36,26 @@ const expenseData = [
   },
 ];
 
-App.get("/data", (req, res) => {
-  res.status(200).json(expenseData);
+App.get("/data", async (req, res) => {
+  const data = await exp.find();
+  console.log(data);
+  res.status(200).json(data);
 });
 
-App.post("/data", (req, res) => {
+App.post("/data", async (req, res) => {
   const { amount, type, title, date, description } = req.body;
-  const newExpense = {
-    amount,
-    type,
-    title,
-    date,
-    description,
-  };
-  expenseData.push(newExpense);
-  res.status(201).json(`data stored ${newExpense}`);
+  const newExp = new exp({ amount, type, title, date, description });
+  await newExp.save();
+
+  //   const newExpense = {
+  //     amount,
+  //     type,
+  //     title,
+  //     date,
+  //     description,
+  //   };
+  //   expenseData.push(newExpense);
+  res.status(201).json(`data stored `);
 });
 
 const PORT = 8000;
